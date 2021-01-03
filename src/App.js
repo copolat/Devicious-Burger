c38ffc9112b549e6adb256422736a50a8c196a8b
unknown <copolat@yahoo.comcopolat git config --global user.name copolat>
2021-01-03 14:56:56 -0600

    Convert App component into a class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'; 
+import logo from './burger-logo.png';
 import './App.css';
+import Login from './components/Login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state= {
+
+    } 
+  }
+  render(){
+    return (
+      <div className="App">
+        <img src={logo} alt="Delicious Burger" id="logo" />
+        <hr></hr>
+        <Login />
+      </div>
+    );
+  }
+
 }
 
 export default App;
